eac7c4f23dd722acaf96cd86114cf6d586a6c203
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-02 02:53:50 -0400

    added hideElements function and hide color label and menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 var $name = $('#name'); //input field for the name.
 var $otherTitle = $('#other-title');
-$otherTitle.hide();
+var $colorMenu = $('#color');
 
 
 // function below, when page loads, gives focus to the first text field 
 function setFocus(){
     $name.focus();
+    hideElements();
+}
+
+function hideElements(){
+    $otherTitle.hide();
+    $labelColor.hide();
+    $colorMenu.hide();
 }
 
 //function below shows an input of type text when a user selects other from
@@ -14,6 +21,8 @@ function createOtherRole(){
     for(var i =0; i < $('#title').children().length; i++){ // a loop for all job roles
         if($('#title option:selected').eq(i).text() === 'Other'){ // only for selected option
             $otherTitle.show(); // change the state of the text field from hide to show.
+        }else{
+            //$otherTitle.hide();
         }
     }
 }
