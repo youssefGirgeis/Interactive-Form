4dcaefafecc30589df8a60cf72037231c1f1486b
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-02 02:20:58 -0400

    added a function: createOtherRole
    when a user selects other from the Job role drop down menu, a text
    field appears.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 var $name = $('#name'); //input field for the name.
+var $otherTitle = $('#other-title');
+$otherTitle.hide();
+
 
 // function below, when page loads, gives focus to the first text field 
 function setFocus(){
     $name.focus();
 }
+
+//function below shows an input of type text when a user selects other from
+// the job role drop down menu.
+function createOtherRole(){
+    for(var i =0; i < $('#title').children().length; i++){ // a loop for all job roles
+        if($('#title option:selected').eq(i).text() === 'Other'){ // only for selected option
+            $otherTitle.show(); // change the state of the text field from hide to show.
+        }
+    }
+}
+
+
+$('#title').on('change', createOtherRole);
